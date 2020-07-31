4a6936bd1231cad85f94c18420d1c10383b1609e
Victor Tran <60512497+VictorTran-rc@users.noreply.github.com>
2020-07-31 09:21:39 -0400

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,28 +1,42 @@
 const express = require('express');
+const axios = require('axios');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator');
+const {
+    check,
+    validationResult
+} = require('express-validator');
+// bring in normalize to give us a proper url, regardless of what user entered
+const normalize = require('normalize-url');
+// const checkObjectId = require('../../middleware/checkObjectId');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
-// @Route GET api/profile/me
-// @desc | Get current users profile
-// @access Private
+const Post = require('../../models/Post');
 
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
 router.get('/me', auth, async (req, res) => {
-  try {
-    const profile = await (
-      await Profile.findOne({ user: req.user.id })
-    ).populated('user', ['name', 'avatar']);
-    if (!profile) {
-      return res.status(400).json({ msg: 'There is no profile for this user' });
+    try {
+        const profile = await Profile.findOne({
+            user: req.user.id
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({
+                msg: 'There is no profile for this user'
+            });
+        }
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
     }
-    res.json(profile);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).send('Server Error');
-  }
 });
+
 // @route    POST api/profile
 // @desc     Create or update user profile
 // @access   Private
@@ -66,8 +80,40 @@ router.post(
         if (status) profileFields.status = status;
         if (skills) {
             profileFields.skills = skills.split(',').map(skill => skill.trim());
-        }
-    }
-);
+        };
 
+        // Build social object and add to profileFields
+        profileFields.social = {}
+        if (youtube) profileFields.social.youtube = youtube;
+        if (twitter) profileFields.social.twitter = twitter;
+        if (facebook) profileFields.social.facebook = facebook;
+        if (linkedin) profileFields.social.linkedin = linkedin;
+        if (instagram) profileFields.social.instagram = instagram;
+
+        try {
+            let profile = await Profile.findOne({
+                user: req.user.id
+            })
+
+            if (profile) {
+                //update
+                profile = await Profile.findOneAndUpdate({
+                    user: req.user.id
+                }, {
+                    $set: profileFields
+                }, {
+                    new: true
+                });
+                return res.json(profile)
+            }
+            //Create 
+            profile = new Profile(profileFields);
+            await profile.save()
+            res.json(profile)
+
+        } catch (err) {
+            console.error(err.message)
+            res.status(500).send('Server Error')
+        }
+    });
 module.exports = router;
